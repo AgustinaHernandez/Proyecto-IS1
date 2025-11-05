d32c6e80bb68ac03735130456de13f7288eb995e
Francine Pagliasso Pelliciotti <franpelliciotti00@gmail.com>
2025-11-05 12:42:26 -0300

    Se agrega scheme.sql (Base de datos)

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -7,3 +7,34 @@ CREATE TABLE users (
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
 );
+
+DROP TABLE IF EXISTS persons;
+
+-- Creación de tabla 'persons'
+CREATE TABLE persons (
+    id INTEGER NOT NULL PRIMARY KEY,
+    dni INTEGER NOT NULL UNIQUE,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS teachers;
+
+-- Creación de tabla 'teachers'
+CREATE TABLE teachers (
+    id INTEGER NOT NULL PRIMARY KEY,
+    person_id INTEGER NOT NULL,
+    degree TEXT NOT NULL,
+    email TEXT NOT NULL,
+
+    CONSTRAINT fk_id_teacher FOREIGN KEY person_id REFERENCES persons (id)
+);
+
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+    id INTEGER NOT NULL PRIMARY KEY,
+    person_id INTEGER NOT NULL,
+
+    CONSTRAINT fk_id_student FOREIGN KEY person_id REFERENCES persons (id)
+);
